1c1785aa34bcff48ffb729b6f9a5a3cf4b69b6ed
AyariAhmed <ayari17@hotmail.fr>
2022-07-11 18:23:02 +0100

    utils, routes, config

---
+++ pkg/config/app.go
@@ -0,0 +1,19 @@
+package config
+
+import "github.com/jinzhu/gorm"
+
+var (
+	db *gorm.DB
+)
+
+func Connect() {
+	d, err := gorm.Open("mysql", "ahmed:root@tcp(127.0.0.1:3306)/bookstore-crud?charset=utf8mb4&parseTime=True&loc=Local")
+	if err != nil {
+		panic(err)
+	}
+	db = d
+}
+
+func GetDB() *gorm.DB {
+	return db
+}
