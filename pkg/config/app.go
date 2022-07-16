651d587c2b6c4ba07c79382ba764fa0aaf28436b
AyariAhmed <ayari17@hotmail.fr>
2022-07-16 01:53:50 +0100

    fix gorm database connection

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -1,17 +1,25 @@
 package config
 
-import "github.com/jinzhu/gorm"
+import (
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"log"
+)
 
 var (
 	db *gorm.DB
 )
 
 func Connect() {
-	d, err := gorm.Open("mysql", "ahmed:root@tcp(127.0.0.1:3306)/bookstore-crud?charset=utf8mb4&parseTime=True&loc=Local")
+	dsn := "ahmed:root@tcp(127.0.0.1:3306)/bookstore-crud?charset=utf8mb4&parseTime=True&loc=Local"
+	d, err := gorm.Open("mysql", dsn)
 	if err != nil {
+		log.Println("Connection Failed to Open")
 		panic(err)
 	}
 	db = d
+	log.Println("Connection Established")
 }
 
 func GetDB() *gorm.DB {
