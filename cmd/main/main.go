c8cb3080f085007dbdbecdb18459ae35b7e628ba
AyariAhmed <ayari17@hotmail.fr>
2022-07-16 01:56:33 +0100

    build the project

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/AyariAhmed/bookstore-crud/pkg/routes"
 	"github.com/gorilla/mux"
 	"log"
@@ -11,5 +12,6 @@ func main() {
 	r := mux.NewRouter()
 	routes.RegisterBookStoreRoutes(r)
 	http.Handle("/", r)
+	fmt.Println("Starting server on port 9010 (http://localhost:9010)")
 	log.Fatal(http.ListenAndServe("localhost:9010", r))
 }
